93c31aa3bd6f6d67066b56c3c2841affeb5a7c38
Anjali Kumari <anjali.kumari@nutanix.com>
2026-06-02 14:24:58 +0530

    Cloud native refactor of app. and other corrections

--- Fake_News_Detector_Project/App/backend/app.py
+++ Fake_News_Detector_Project/App/backend/app.py
@@ -62,4 +62,8 @@ def predict():
         "prediction": result })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
